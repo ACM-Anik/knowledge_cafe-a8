f663d0ebedacd27d0488a0e3f8377eec08b84690
ACM-Anik <anikmojumder5252@gmail.com>
2023-03-31 23:45:27 +0600

    Question no.1 added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Blogs from './components/Blogs/Blogs';
 import Header from './components/Header/Header';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Questions from './components/Questions/Questions';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header></Header>
        <Blogs></Blogs>
+       <Questions></Questions>
        <ToastContainer></ToastContainer>
     </div>
   )
--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -3,8 +3,22 @@ import './Questions.css';
 
 const Questions = () => {
     return (
-        <div>
-            
+        <div className="my-5">
+            <div>
+                <h1 className="text-center text-4xl font-bold ">Frequently Common <span className="text-[#6047EC]">Questions</span></h1>
+            </div>
+            <div className="my-6 p-4 md:p-8 rounded-lg bg-[#1111110D]">
+                <div className="bg-white rounded-lg p-4 my-4">
+                    <h1 className="text-2xl font-bold ">1. Props vs state:-</h1>
+                    <p className="text-lg font-semibold">Props: <br />
+                        Props are read-only and immutable, allow you to pass data from one component to other components as an argument. Props can be accessed by the child component and are used to communicate between components, make components reusable.
+                    </p>
+                    <p className="text-lg font-semibold">State: <br />
+                        State changes can be asynchronous. It is mutable. It holds information about the components. It cannot be accessed by child components. It can be used for rendering dynamic changes with the component and cannot make components reusable.
+                    </p>
+                </div>
+                
+            </div>
         </div>
     );
 };
